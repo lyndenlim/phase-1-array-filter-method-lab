166baacd6e4ee4364ea13286e5f12bf342c8d6d6
Lynden Lim <lyndenlim15@gmail.com>
2022-01-11 20:43:01 -0500

    commit tree 806133e60900b980e7b6f0e28b4a68c388f0ccd3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string){
+    return array.filter(element => element.toLowerCase() === string.toLowerCase());
+};
+
+function fuzzyMatch(array, string){
+    return array.filter(function(element){
+        if (element.indexOf(string) === 0){
+            return element;
+        };
+    });
+};
+
+function matchName(drivers, string){
+    return drivers.filter(element => element.name.toLowerCase() === string.toLowerCase());
+};
